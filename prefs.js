25261ae6dadfc32d15658cc8a1ea57258d022b1c
marschr <marcosscheeren@gmail.com>
2025-03-25 15:31:03 -0300

    Added MAC auto detect from IP

--- prefs.js
+++ prefs.js
@@ -38,6 +38,58 @@ export default class HomeServerPreferences extends ExtensionPreferences {
         serverMACRow.connect('changed', entry => {
             settings.set_string('servermac', entry.get_text());
         });
+        
+        // Add Detect MAC button
+        const detectButton = new Gtk.Button({
+            label: _('Detect MAC'),
+            valign: Gtk.Align.CENTER,
+            css_classes: ['suggested-action'],
+            margin_start: 8,
+            margin_end: 8,
+        });
+        
+        detectButton.connect('clicked', () => {
+            const ip = settings.get_string('serverip');
+            if (!ip || ip === '') {
+                this._showError(_('Please enter a valid IP address first'));
+                return;
+            }
+            
+            // Try to detect MAC address using ip neigh command
+            try {
+                const proc = Gio.Subprocess.new(
+                    ['ip', 'neigh', 'show', ip],
+                    Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+                );
+                
+                proc.communicate_utf8_async(null, null, (proc, res) => {
+                    try {
+                        const [, stdout, stderr] = proc.communicate_utf8_finish(res);
+                        if (proc.get_successful()) {
+                            // Parse output to find MAC address
+                            const macMatch = stdout.match(/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/);
+                            if (macMatch && macMatch[0]) {
+                                const macAddress = macMatch[0].toLowerCase();
+                                serverMACRow.set_text(macAddress);
+                                settings.set_string('servermac', macAddress);
+                                this._showSuccess(_('MAC address detected and applied'));
+                            } else {
+                                // Try alternative with arp command
+                                this._detectWithArp(ip, serverMACRow, settings);
+                            }
+                        } else {
+                            this._detectWithArp(ip, serverMACRow, settings);
+                        }
+                    } catch (e) {
+                        this._detectWithArp(ip, serverMACRow, settings);
+                    }
+                });
+            } catch (e) {
+                this._detectWithArp(ip, serverMACRow, settings);
+            }
+        });
+        
+        serverMACRow.add_suffix(detectButton);
         group.add(serverMACRow);
         
         // Server Poll
@@ -85,5 +137,58 @@ export default class HomeServerPreferences extends ExtensionPreferences {
             subtitle: version
         });
         versionGroup.add(versionRow);
+    }
+    
+    _detectWithArp(ip, serverMACRow, settings) {
+        try {
+            const proc = Gio.Subprocess.new(
+                ['arp', '-n', ip],
+                Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+            );
+            
+            proc.communicate_utf8_async(null, null, (proc, res) => {
+                try {
+                    const [, stdout, stderr] = proc.communicate_utf8_finish(res);
+                    if (proc.get_successful()) {
+                        // Parse output to find MAC address
+                        const macMatch = stdout.match(/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/);
+                        if (macMatch && macMatch[0]) {
+                            const macAddress = macMatch[0].toLowerCase();
+                            serverMACRow.set_text(macAddress);
+                            settings.set_string('servermac', macAddress);
+                            this._showSuccess(_('MAC address detected and applied'));
+                        } else {
+                            this._showError(_('Could not detect MAC address for the given IP'));
+                        }
+                    } else {
+                        this._showError(_('Could not detect MAC address for the given IP'));
+                    }
+                } catch (e) {
+                    this._showError(_('Error detecting MAC address: ') + e.message);
+                }
+            });
+        } catch (e) {
+            this._showError(_('Error executing ARP command: ') + e.message);
+        }
+    }
+    
+    _showError(message) {
+        const dialog = new Adw.MessageDialog({
+            heading: _('Error'),
+            body: message,
+            modal: true,
+        });
+        dialog.add_response('ok', _('OK'));
+        dialog.present();
+    }
+    
+    _showSuccess(message) {
+        const dialog = new Adw.MessageDialog({
+            heading: _('Success'),
+            body: message,
+            modal: true,
+        });
+        dialog.add_response('ok', _('OK'));
+        dialog.present();
     }
 } 
